e145e8758b80fbcb253e978662b7e50cc987cd12
NFox1 <sarmiento_lencina@proton.me>
2024-09-16 17:40:35 -0300

    1
    Lo que se llego a hacer en laptop (persona.cs incompleto)

---
+++ Persona.cs
@@ -0,0 +1,9 @@
+﻿class Persona
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public int DNI { get; set; }
+    public char Sexo { get; set; }
+    public decimal Peso { get; set; }
+    public decimal Altura { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("Aplicacion de consola: Trabajo Practico 1 - C# Basics\n" + "Elija el programa:\n" + "1. Cuenta Bancaria | 2. Persona | 3. Electrodomestico");
+        decimal.TryParse(Console.ReadLine(), out decimal AplicacionSeleccionada);
+        if (AplicacionSeleccionada == 1)
+        {
+            Cuenta cuenta = new Cuenta("Marcos", 1000);
+            Console.WriteLine("Bienvenido " + cuenta.Titular + ", indique si va a realizar un deposito (ingresando 1) o una extraccion (ingresando 2)");
+            Console.WriteLine("Su saldo actual es de: " + cuenta.Cantidad);
+            decimal.TryParse(Console.ReadLine(), out decimal OperacionElegida);
+            if (OperacionElegida == 1)
+            {
+                Console.WriteLine("Por favor ingrese la cantidad a ingresar");
+                decimal.TryParse(Console.ReadLine(), out decimal SaldoPorAgregar);
+                cuenta.Deposito(SaldoPorAgregar);
+                Console.WriteLine(cuenta.MostrarSaldo());
+            }
+            else
+            {
+                if (OperacionElegida == 2)
+                {
+                    Console.WriteLine("Por favor ingrese la cantidad a retirar");
+                    decimal.TryParse(Console.ReadLine(), out decimal SaldoPorRetirar);
+                    cuenta.Retiro(SaldoPorRetirar);
+                    Console.WriteLine(cuenta.MostrarSaldo());
+                }
+                else
+                {
+                    Console.WriteLine("Invalido");
+                }
+            }
+        }
+        else
+        {
+            if (AplicacionSeleccionada == 2)
+            {
+                Console.WriteLine("En desarrollo");
+            }
+            else
+            {
+                if (AplicacionSeleccionada == 3)
+                {
+                    Console.WriteLine("En desarrollo");
+                }
+                else
+                {
+                    Console.WriteLine("Invalido");
+                }
+            }
+        }
+        
+    }
+}
